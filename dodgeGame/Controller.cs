182333fb0c98a988197181494d66f08d311c5abb
charlie-0001 <charbee.qwq@gmail.com>
2026-08-12 23:38:38 -0230

    Created an abstract controller class and a player controller.

---
+++ dodgeGame/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodgeGame
+{
+    abstract internal class Controller(List<Enum> actions, Dictionary<Enum, Action> movements)
+    {
+        public List<Enum> Actions { get; set; } = actions;
+        public Dictionary<Enum, Action> Movements { get; set; } = movements;
+    }
+}
---
+++ dodgeGame/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace dodgeGame
+{
+    internal class PlayerController : Controller
+    {
+        public PlayerController() : base([Keys.W, Keys.S, Keys.A, Keys.D], new Dictionary<Enum, Action>{}) { }
+
+        public void BindPlayer(Player player)
+        {
+            Movements[Keys.A] = () =>
+            {
+                if (player.Velocity.X > -player.MaxSpeed)
+                {
+                    player.Velocity += new Vector2(-player.Speed, 0);
+                }
+            };
+
+            Movements[Keys.D] = () =>
+            {
+                if (player.Velocity.X < player.MaxSpeed)
+                {
+                    player.Velocity += new Vector2(player.Speed, 0);
+                }
+            };
+
+            Movements[Keys.W] = () =>
+            {
+                if (player.Velocity.Y > -player.MaxSpeed)
+                {
+                    player.Velocity += new Vector2(0, -player.Speed);
+                }
+            };
+
+            Movements[Keys.S] = () =>
+            {
+                if (player.Velocity.Y < player.MaxSpeed)
+                {
+                    player.Velocity += new Vector2(0, player.Speed);
+                }
+            };
+        }
+    }
+}
+
