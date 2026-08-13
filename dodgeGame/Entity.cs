605ba1712c551fb1ccfca87dfbe71a5888c58228
charlie-0001 <charbee.qwq@gmail.com>
2026-08-12 23:37:40 -0230

    Created an entity class that all enemies, projectiles, and players will have.

---
+++ dodgeGame/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodgeGame
+{
+    abstract internal class Entity(Sprite sprite, Controller controller)
+    {
+        public Sprite sprite { get; set; } = sprite;
+        public Controller controller { get; set; } = controller;
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
