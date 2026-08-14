7a5e513856095da3b6d67146e628624c16095cdc
charlie-0001 <charbee.qwq@gmail.com>
2026-08-14 21:09:10 -0230

    Created unfinished wall class.

---
+++ dodgeGame/Entities/Wall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodgeGame.Entities
+{
+    internal class Wall(Sprite sprite) : Entity(sprite, kills: false, isActive: false)
+    {
+    }
+}
