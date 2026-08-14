2c6cce1de4ef98395ee0163bbdbee6f138a8d13b
charlie-0001 <charbee.qwq@gmail.com>
2026-08-14 19:04:57 -0230

    Added an enemy controller class.

---
+++ dodgeGame/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodgeGame
+{
+    internal class EnemyController() : Controller([Actions.NOTHING], new Dictionary<Enum, Action> { })
+    {
+        private enum Actions
+        {
+            NOTHING
+        }
+    }
+}
