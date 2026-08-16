ecf322edba9dd756d0506834f7dea0a0ce1d4407
charlie-0001 <charbee.qwq@gmail.com>
2026-08-16 13:29:58 -0230

    Created wall class.

--- dodgeGame/Entities/Wall.cs
+++ dodgeGame/Entities/Wall.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dodgeGame.Entities
 {
-    internal class Wall(Sprite sprite) : Entity(sprite, kills: false, isActive: false)
+    internal class Wall(Sprite sprite) : Entity(sprite, kills: false, isActive: true)
     {
+
     }
 }
